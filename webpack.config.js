a24d5d7f78f52a28ebf15bdeb9897d7eec6ad4bc
Mark Howells-Mead <permanent.tourist@gmail.com>
2019-03-22 17:55:45 +0100

    Auto-tidy config file

--- webpack.config.js
+++ webpack.config.js
@@ -5,48 +5,48 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const extractSass = new MiniCssExtractPlugin({
-  filename: '[name].min.css'
+	filename: '[name].min.css'
 });
 
 const uglifyJS = new UglifyJSPlugin();
 
 module.exports = {
-  entry: {
-    blocks: path.resolve(__dirname, 'blocks/src/blocks'),
-  },
-  plugins: [
-    uglifyJS,
-    extractSass,
+	entry: {
+		blocks: path.resolve(__dirname, 'blocks/src/blocks'),
+	},
+	plugins: [
+	uglifyJS,
+	extractSass,
   ],
-  optimization: {
-    minimizer: [
-      uglifyJS,
-    ]
-  },
-  output: {
-    filename: '[name].min.js',
-    path: path.resolve(__dirname, 'blocks/dist')
-  },
-  module: {
-    rules: [{
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.scss$/,
-        exclude: /(node_modules)/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          'sass-loader',
-        ]
-      }
-    ]
-  },
-  watchOptions: {
-    poll: true,
-    ignored: /node_modules/
-  }
+	optimization: {
+		minimizer: [
+	  uglifyJS,
+	]
+	},
+	output: {
+		filename: '[name].min.js',
+		path: path.resolve(__dirname, 'blocks/dist')
+	},
+	module: {
+		rules: [{
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules)/,
+				loader: 'babel-loader'
+	  },
+			{
+				test: /\.scss$/,
+				exclude: /(node_modules)/,
+				use: [
+		  MiniCssExtractPlugin.loader,
+		  'css-loader',
+		  'postcss-loader',
+		  'sass-loader',
+		]
+	  }
+	]
+	},
+	watchOptions: {
+		poll: true,
+		ignored: /node_modules/
+	}
 };
